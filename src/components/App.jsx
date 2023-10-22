0100747eb48d73ec726137057ac1577443b1b006
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2023-10-22 09:09:15 +0300

    update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { AddTodoAction, RemoveTodoAction } from './actions/TodoActions';
+import { useState } from 'react';
+
+function App() {
+  const [todo, setTodo] = useState();
+
+  const dispatch = useDispatch();
+
+  const Todo = useSelector(state => state.Todo);
+  const { todos } = Todo;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(AddTodoAction(todo));
+  };
+  const removeHandeler = t => {
+    dispatch(RemoveTodoAction(t));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <header className="App-header">
+        <h2>ToDo List App</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Enter a ToDo"
+            autoComplete="off"
+            type="text"
+            name="todo"
+            style={{
+              width: 350,
+              padding: 10,
+              borderRadius: 20,
+              border: 'none',
+              fontSize: 20,
+            }}
+            onChange={e => setTodo(e.target.value)}
+          />
+          <button type="submit">Go</button>
+        </form>
+        <ul className="allTodos">
+          {todos &&
+            todos.map(t => (
+              <li key={t.id} className="singleTodo">
+                <span className="todoText">{t.todo}</span>
+                <button
+                  style={{
+                    borderRadius: 25,
+                    padding: 10,
+                    border: '1px solid white',
+                    color: 'white',
+                    backgroundColor: 'orangered',
+                  }}
+                  onClick={() => removeHandeler(t)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+        </ul>
+      </header>
     </div>
   );
-};
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './components/store';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
